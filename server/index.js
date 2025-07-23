af56580a93ac5d6bd4aa720e76b25740e78b074d
Robin Singh <vougishtiger@gmail.com>
2025-07-23 03:48:34 -0700

    main

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@
 const express= require("express");
 const cors= require("cors");
 const pool= require("./db");
+const bcrypt= require("bcryptjs");
+const jwt= require("jsonwebtoken");
+const SECRET= process.env.JWT_SECRET || "supersecretkey"; 
 require("dotenv").config();
 
 const app= express();
@@ -81,6 +84,50 @@ app.delete("/api/favorites/:id", async(req,res)=> {
   } catch(err) {
     console.error("Failed to delete favorite", err);
     res.status(500).json({error:"Failed to delete Favorite"});
+  }
+});
+
+app.post("/api/auth/register", async(req,res)=> {
+  const {name, email, password}= req.body;
+
+  try{
+    const hashedPassword= await bcrypt.hash(password, 10);
+
+    const user= await pool.query(
+      "INSERT INTO users (name, email, password) VALUES($1, $2, $3) RETURNING id, name, email",
+      [name, email, hashedPassword]
+    );
+
+    res.status(201).json(user.rows[0]);
+  } catch(err) {
+    console.error("Registration error:",err);
+    res.status(500).json({error:"Email already in use or registration failed"});
+  }
+});
+
+app.post("/api/auth/login", async(req,res)=> {
+  const {email,password}= req.body;
+
+  try{
+    const userRes= await pool.query("SELECT * FROM users WHERE email= $1", [email]);
+
+    if (userRes.rows.length===0) {
+      return res.status(401).json({error: "Invalid credentials"});
+    }
+
+    const user= userRes.rows[0];
+    const isMatch= await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({error: "Invalid credentials"});
+    }
+    
+    const token= jwt.sign({id: user.id, email: user.email}, SECRET, {exppiresIn:"7d"});
+
+     res.json({ token, user: { id: user.id, name: user.name, email: user.email } });
+  } catch (err) {
+    console.error("Login error:", err);
+    res.status(500).json({ error: "Login failed" });
   }
 });
 
